c1f30ecc8b5562326fdc0429ea5e685c7643504d
Haruta Tasaki <hrtune@gmail.com>
2022-10-21 19:51:44 +0900

    Add a Profile model

--- social_book/core/models.py
+++ social_book/core/models.py
@@ -1,3 +1,12 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user = models.IntegerField()
+    bio = models.TextField(blank=True)
+    profileimg = models.ImageField(upload_to='profile_images', default='blank_profile_picture.png')
+    location = models.CharField(max_length=128, blank=True)
