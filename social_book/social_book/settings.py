1c94a6952e370d1bcbf2dd444690a2d0edd58381
Haruta Tasaki <hrtune@gmail.com>
2022-10-21 17:03:16 +0900

    Hide the SECRET_KEY

--- social_book/social_book/settings.py
+++ social_book/social_book/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import decouple
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-zb@m8r#9w1c-m5wlc3jr)u*e8dh183^1r+ve4@zsby%h!btru#"
 
 # SECURITY WARNING: don't run with debug turned on in production!
+SECRET_KEY = decouple.config('SECRET_KEY')
+
 DEBUG = True
 
 ALLOWED_HOSTS = []
