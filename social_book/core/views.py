5d59f521f29c12619be936dc59f4cef3947b176e
Haruta Tasaki <hrtune@gmail.com>
2022-10-22 19:57:17 +0900

    Implement sign in view

--- social_book/core/views.py
+++ social_book/core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User, auth
-from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib import messages, auth
 from .models import Profile
 
 def index(request):
@@ -40,4 +40,17 @@ def signup(request):
     return render(request, 'signup.html')
 
 def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None: # if authenticated,
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credentials Invalid')
+            return redirect('signin')
+
     return render(request, 'signin.html')
