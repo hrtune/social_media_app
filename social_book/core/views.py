a71fadb8ff7572719b06c4215f606bd2fc907fe6
Haruta Tasaki <hrtune@gmail.com>
2022-10-27 20:15:51 +0900

    Fix bug, not to save posted images

--- social_book/core/views.py
+++ social_book/core/views.py
@@ -31,6 +31,7 @@ def settings(request):
         user_profile.user.email = email
         user_profile.bio = bio
         user_profile.location = location
+        user_profile.profileimg = image
         user_profile.save()
 
         return redirect(request.path)
